b7214feca4ba3a37464f894ebad9fc9ee03ccf9d
Esila-A <esila.adajewa@gmail.com>
2022-09-25 08:57:30 +0200

    added read me file

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,13 @@
+import { HashLink, NavHashLink } from "react-router-hash-link";
+
 const Navigation = (props) => {
   return (
     <nav className="navigation">
-      <a href="#landing" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Główna</a>
-      <a href="#services" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Usługi</a>
-      <a href="#about" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>O nas</a>
-      <a href="#contact" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Kontakt</a>
+      <HashLink to="/#landing" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Główna</HashLink>
+      <HashLink to="/#services" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Usługi</HashLink>
+      <NavHashLink to="/examples/#examples" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Nasze prace</NavHashLink>
+      <HashLink to="/#about" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>O nas</HashLink>
+      <HashLink to="/#contact" className="navigation_link" onClick={() => props.isMobilePhone && props.closeMenu()}>Kontakt</HashLink>
     </nav>
   );
 };
